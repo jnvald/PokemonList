cadcc04dbbd908b992fe9964b054af2bc82fcc23
jnvald <jnvald@gmail.com>
2021-07-14 20:15:51 -0400

    Ejercicio Listar Pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PokemonCard from './component/PokemonCard';
+
+let pokemon = [
+  { "id": 1, "name": "Bulbasaur", "types": ["poison", "grass"] },
+  { "id": 5, "name": "Charmeleon", "types": ["fire"] },
+  { "id": 9, "name": "Blastoise", "types": ["water"] },
+  { "id": 12, "name": "Butterfree", "types": ["bug", "flying"] },
+  { "id": 16, "name": "Pidgey", "types": ["normal", "flying"] },
+  { "id": 23, "name": "Ekans", "types": ["poison"] },
+  { "id": 24, "name": "Arbok", "types": ["poison"] },
+  { "id": 25, "name": "Pikachu", "types": ["electric"] },
+  { "id": 37, "name": "Vulpix", "types": ["fire"] },
+  { "id": 52, "name": "Meowth", "types": ["normal"] },
+  { "id": 63, "name": "Abra", "types": ["psychic"] },
+  { "id": 67, "name": "Machamp", "types": ["fighting"] },
+  { "id": 72, "name": "Tentacool", "types": ["water", "poison"] },
+  { "id": 74, "name": "Geodude", "types": ["rock", "ground"] },
+  { "id": 87, "name": "Dewgong", "types": ["water", "ice"] },
+  { "id": 98, "name": "Krabby", "types": ["water"] },
+  { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+  { "id": 122, "name": "Mr. Mime", "types": ["psychic"] },
+  { "id": 133, "name": "Eevee", "types": ["normal"] },
+  { "id": 144, "name": "Articuno", "types": ["ice", "flying"] },
+  { "id": 145, "name": "Zapdos", "types": ["electric", "flying"] },
+  { "id": 146, "name": "Moltres", "types": ["fire", "flying"] },
+  { "id": 148, "name": "Dragonair", "types": ["dragon"] }
+];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="pc">
+       {     
+       pokemon.map((poke) => {
+         <PokemonCard perro={poke.id}/>
+         console.log("perro")
+       })
+
+      } 
+      </div>
     </div>
   );
 }
---
+++ src/component/PokemonCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PokemonCard = (props) => {
+
+
+    return (
+        <div>
+            <div>
+                <h1>Nombre : {this.props.perro}</h1>
+                <h3>Id: {}</h3>
+                <h3>Type: {}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard
